a3f2fb1cc2b17fa3b8762c51f0fedf1c4730613c
vikram <vikramkunwar1404@gmail.com>
2023-08-01 16:42:28 +0530

    Initial Commit

---
+++ AnimateSplashScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimateSplashScreen
+//
+//  Created by Vikram Kunwar on 01/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
